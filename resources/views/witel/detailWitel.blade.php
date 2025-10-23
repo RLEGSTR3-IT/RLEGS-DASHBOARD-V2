1f696194450786291e2e6b8434e0a018fd3a0bea
PORTODIT <adhityapmp@gmail.com>
2025-10-23 09:55:27 +0700

    added detail witel

--- resources/views/witel/detailWitel.blade.php
+++ resources/views/witel/detailWitel.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.main')
 
-@section('title', 'Detail Witel')
+@section('title', 'Detail Witel RLEGS TR3')
 
 @section('styles')
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.14.0-beta3/dist/css/bootstrap-select.min.css">

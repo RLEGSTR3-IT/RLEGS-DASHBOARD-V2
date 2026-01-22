dc4a529b457c551c490b2692e3c37a1ab778db53
Adit <adhityapmp@gmail.com>
2026-01-22 10:17:01 +0700

    fixed detail am feature

--- resources/views/am/detailAM.blade.php
+++ resources/views/am/detailAM.blade.php
@@ -4,7 +4,7 @@
 
 @section('styles')
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.14.0-beta3/dist/css/bootstrap-select.min.css">
-<link rel="stylesheet" href="{{ asset('css/detailAM.css') }}">
+<link rel="stylesheet" href="{{ asset('css/detailam.css') }}">
 @endsection
 
 @section('content')
